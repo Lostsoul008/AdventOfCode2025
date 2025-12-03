ce37dc0da739979e8da09a0a6955acf8043061ee
Lostsoul008 <ubryanlamp@gmail.com>
2025-12-02 20:10:33 -0500

    Day 2 Part 2 Complete

--- Days/Day2/day2.py
+++ Days/Day2/day2.py
@@ -27,13 +27,38 @@ def get_invalid(ranges: List[tuple[int, int]]) -> List[int]:
 	return result
 
 
-def sum_invalid(values: List[int]) -> int:
-	return sum(values)
+def get_invalid_arbitrary(ranges: List[tuple[int, int]]) -> List[int]:
+	result: List[int] = []
+	for start, end in ranges:
+		for i in range(start, end + 1):
+			str_i = str(i)
+			length = len(str_i)
+			mid = length // 2
+
+			for sub_len in range(1, mid + 1):
+				if length % sub_len != 0:
+					continue
+
+				sub_string = str_i[:sub_len]
+				total = length // sub_len
+
+				is_repeating = True
+				for j in range(1, total):
+					if sub_string != str_i[j * sub_len : (j+1) * sub_len]:
+						is_repeating = False
+						break
+
+				if is_repeating:
+					result.append(i)
+					break
+
+	return result
 
 
 def main() -> None:
 	file_input = getLines()
-	print(f'{sum_invalid(get_invalid(get_ranges(file_input))) = }')
+	print(f'{sum(get_invalid(get_ranges(file_input))) = }')
+	print(f'{sum(get_invalid_arbitrary(get_ranges(file_input))) = }')
 
 
 if __name__ == '__main__':
