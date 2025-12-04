fc2209b1819a3b444b94b934f7b6d2af4582f2d0
Lostsoul008 <ubryanlamp@gmail.com>
2025-12-03 22:31:37 -0500

    Day 3 Part 2 Complete

--- src/day3.py
+++ src/day3.py
@@ -1,21 +1,32 @@
 from file_helpers import Lines, get_lines
 
 
-def get_max_battery(batteries: str):
+def get_max_battery(batteries: str, total: int = 2):
 	li_batteries: list[int] = [int(c) for c in batteries]
-	maximum: int = 0
+	length = len(batteries)
+	start = 0
+	result = 0
 
-	for i, battery in enumerate(li_batteries):
-		for second_battery in li_batteries[i + 1:]:
-			maximum = max(maximum, battery * 10 + second_battery)
+	while total > 0:
+		total -= 1
+		largest_digit = 0
+		largest_index = 0
 
-	return maximum
+		for i in range(start, length - total):
+			if li_batteries[i] > largest_digit:
+				largest_digit = li_batteries[i]
+				largest_index = i
+
+		start = largest_index + 1
+		result += 10 ** total * li_batteries[largest_index]
+
+	return result
 
 
-def get_total_battery(lines: Lines):
+def get_total_battery(lines: Lines, total_bat: int = 2):
 	total: int = 0
 	for line in lines:
-		total += get_max_battery(line)
+		total += get_max_battery(line, total_bat)
 
 	return total
 
@@ -23,6 +34,7 @@ def get_total_battery(lines: Lines):
 def main() -> None:
 	file_input = get_lines(3)
 	print(f'Part 1 = {get_total_battery(file_input)}')
+	print(f'Part 2 = {get_total_battery(file_input, 12)}')
 
 
 if __name__ == '__main__':
